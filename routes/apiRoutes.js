a50cc013d8bf5a74ee2588bfad8560a14c544386
Andrea Fenderson <andrea_fenderson@ymail.com>
2020-04-23 14:08:28 -0500

    added unique id

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,6 @@
 const db = require("../db.json");
 const fs = require("fs");
-const uuid = require("uuid");
+const uuid = require("uuid/v4");
 
 
 module.exports = function(app) {
@@ -17,19 +17,35 @@ module.exports = function(app) {
         text: req.body.text,
 
       }
-      fs.readFile("../db.json", "utf8", (err,data)=>{
+      fs.readFile("./db.json", "utf8", (err,data)=>{
         if (err) throw err;
         const allNotes = JSON.parse(data)
         //what we are pushing to and then what we are pushing is second//
         allNotes.push(newNote)
 // stringify does the opposit of parse// 2 optional parameters and 1 required, space for readability
-        fs.writeFile("../db.json", JSON.stringify(allNotes,null,2), err=>{
+        fs.writeFile("./db.json", JSON.stringify(allNotes,null,2), err=>{
           if (err) throw err;
           res.send(db)
           console.log  ("note created");
         })
       })
     })
+    //id is the placeholder for the id of the note that we want to delete
+    app.delete("/api/notes/:id", function(req, res) {
+      let noteId = req.params.id;
 
-  
+      fs.readFile("./db.json", "utf8", (err,data)=>{
+        if (err) throw err;
+        const allNotes = JSON.parse(data)
+        //. filter is going to look at all of notes and compare the id passed in the route(line 35) and find the specific note with that id and delete that note
+        //note - is looking at the note element in .json
+        const updatedNotes = allNotes.filter(note=>note.id!= noteId)
+
+        fs.writeFile("./db.json", JSON.stringify(updatedNotes,null,2), err=>{
+          if (err) throw err;
+          res.send(db)
+          console.log  ("note deleted");
+        })
+      })
+    })
   }
--- server.js
+++ server.js
@@ -6,8 +6,9 @@ var PORT = 3000;
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(express.static("public"))
-
+//app is the variable our express server is stored in, so we need to call the variable. need to be below the middleware//
 require ("./routes/htmlRoutes") (app)
+require ("./routes/apiRoutes") (app)
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
